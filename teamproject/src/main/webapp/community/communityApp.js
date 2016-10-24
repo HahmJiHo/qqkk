277080744cf8d6debc91ad044a193a7ff41c6945
s0104 <s0104@NH>
2016-10-24 10:18:53 +0900

    나현 - 커뮤니티

---
+++ teamproject/src/main/webapp/community/communityApp.js
@@ -0,0 +1,57 @@
+$("#loginBtn").click(function (e) {
+	location.href = "../auth/authApp.html"
+})
+
+$("#logoutBtn").click(function (e) {
+	location.href = "../auth/authApp.html"
+})
+
+
+
+function ajaxCommunityList() {
+	$.getJSON(serverAddr + "/community/list.json", function(obj) {
+		var result = obj.jsonResult
+		console.log(result)
+		if (result.state != "success") {
+	    	 alert("서버에서 데이터를 가져오는데 실패했습니다.")
+	    	 return
+	    }
+		
+	    var template = Handlebars.compile($('#trTemplateText').html())
+	    $("#communityTable tbody").html(template(result))
+	    
+	    $(".titleLink").click(function(event) {
+		    window.location.href = "communityForm.html?no=" + $(this).attr("data-no")
+		    
+	    })
+    })
+}
+
+
+
+
+	
+	
+function ajaxLoginUser() {
+	$.getJSON(serverAddr + "/auth/loginUser.json", function(obj) {
+		var result = obj.jsonResult
+	    if (result.state != "success") { // 로그아웃 상태일 경우 로그인 상태와 관련된 태그를 감춘다.
+	         $('.my-login').css("display", "none")
+	         return
+	    }
+	      
+	    $('.my-logout').css("display", "none")
+	      
+	    $("#userName").text(result.data.name);
+	    $("#userName").attr('data-value', result.data.no);
+    })
+}
+
+
+
+
+
+
+
+
+
