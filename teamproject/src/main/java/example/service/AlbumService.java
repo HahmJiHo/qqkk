de562f8761a6a9ff5389ce4ea8e9ef7731e9d606
BitCamp <BitCamp@BitCamp-PC>
2016-11-03 16:08:52 +0900

    tkddnjs

---
+++ teamproject/src/main/java/example/service/AlbumService.java
@@ -0,0 +1,17 @@
+package example.service;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import example.vo.Album;
+
+public interface AlbumService {
+	
+	  List<Album> getAlbumList(int pageNo, int length) throws Exception;
+	  void insertAlbum(Album album, MultipartFile file1, String uploadDir) throws Exception;
+	  List<Album> getAlbum(int no) throws Exception;
+	  int getTotalPage(int pageSize) throws Exception;
+	  void updateAlbum(Album album) throws Exception;
+	  void deleteAlbum(int no) throws Exception;
+}
