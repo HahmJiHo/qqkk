20ee4e814b52b7f58752a1de2d1ad6f4d0e8670f
BitCamp <BitCamp@BitCamp-PC>
2016-11-15 14:22:28 +0900

    samg

--- teamproject/src/main/webapp/album/album02.js
+++ teamproject/src/main/webapp/album/album02.js
@@ -2,13 +2,7 @@ var getGroupNo = $(location).attr('search')
 var groupNo = getGroupNo.split("=")[1]
 groupNo = parseInt(groupNo)
 
-$(".al-btn").click(function(e) { 
-	/*ajaxAddGroup(makegroup)*/
-	console.log("111")
 
-	ajaxAddGroupPhoto()
-
-});
 function ajaxGroupName() { // gno , gsno 불러오는 function
 	$.getJSON(serverAddr +"/album/list.json" , function(obj) {
 		var result = obj.jsonResult
@@ -18,74 +12,86 @@ function ajaxGroupName() { // gno , gsno 불러오는 function
 		} 
 		var contents = ""
 			var contents2 = ""	
-			var arr = result.data.list
-			//	console.log(arr)
-			var template = Handlebars.compile($('#groupNameTemplateText').html())
-			
-			
-			/*for (var i in arr) {
-				$("#groupinfo").attr('data-value', arr[i].groupNo)
-				var sch = arr[i].scheduleList
-				console.log(sch)
-				console.log(sch[i].groupscNo)
-				$("#albumgroupSchedule").attr('data-gsno', sch[i].groupscNo)
-			}*/
-			
-			for (var i in arr) {
-				var sch = arr[i].scheduleList
-				$("#groupinfo").attr('data-value', arr[i].groupNo)								
-				contents +=
-				"<a href='javascript:void(0)' class='w3-text-black' id='groupinfo' data-value=" + arr[i].groupNo+ ">" + arr[i].groupName + "<i class='fa fa-caret-down'></i></a>";
-				for (var j = 0; j < arr[i].scheduleList.length; j++) {
-					
-					if (arr[i].groupNo == arr[i].scheduleList[j].groupNo) {						
-						$("#scheduleList").attr('data-gsno', arr[i].scheduleList[j].groupscNo)							
-						
-						contents += template(arr[i].scheduleList[j])
+				var arr = result.data.list
+				var template = Handlebars.compile($('#groupNameTemplateText').html())
+				for (var i in arr) {
+					var sch = arr[i].scheduleList
+					$("#groupinfo").attr('data-value', arr[i].groupNo)								
+					contents +=
+						"<a href='javascript:void(0)' class='w3-text-black' id='groupinfo' data-value=" + arr[i].groupNo+ ">" + arr[i].groupName + "<i class='fa fa-caret-down'></i></a>";
+					/*contents2 += "<span id='groupinfo' data-value=" + arr[i].groupNo+ ">" + arr[i].groupName + "</span>" + "그룹"*/
+					for (var j = 0; j < arr[i].scheduleList.length; j++) {
+
+						if (arr[i].groupNo == arr[i].scheduleList[j].groupNo) {						
+							$("#scheduleList").attr('data-gsno', arr[i].scheduleList[j].groupscNo)							
+
+							contents += template(arr[i].scheduleList[j])
+						}
 					}
+
+
 				}
-				
-				
-			}
-	//	console.log(arr)
 		$('#albumlist').html(contents)	
-		/*$("#albumlist").html(contents)*/
-		     
+		/*$('#groupNa').html(contents2)*/
+		});
+}
+
+function ajaxGroupNa() {
+	$.getJSON(serverAddr +"/album/list.json" , function(obj) {
+		var result = obj.jsonResult
+		if (result.state != "success") {
+			alert("서버에서 데이터를 가져오는데 실패 했습니다.1111")
+			return
+		} 
+		var arr = result.data.list
+		var contents =""
+			var template = Handlebars.compile($('#groupNameTemplateText2').html())
+		$('body').on('click','#groupinfo', function(e) {
+		var no = $(this).attr('data-value');
+		console.log(no)	
+		for (var i in arr) {
+			if (no == arr[i].groupNo) {
+				contents +=template(arr[i])
+				console.log(contents)
+			}
+		}
+		$('#groupNa').html(contents)	
+	})
 	})
 }
-$(document).ready(function(){
-	$('body').on('click',"#groupinfo", function(){
-	var x = $(this).attr('data-value')
-	console.log(x)
-	var arr = $('.asdf').attr('data-vo')
-	console.log(arr)
-    });
-});
-
 /*window.location.href = "../album/album02.html?no=" + $(this).attr("data-gsno")*/
-function ajaxAddGroupPhoto() {
-	var formData = new FormData();
-	formData.append("memberNo", $("#userName").attr('data-value'));
-	formData.append("groupNo", $("#groupinfo").attr('data-value'));
-	formData.append("groupscNo", $("#showList").attr('data-gsno'));
-	formData.append("file1", $("#file-0a")[0].files[0]);
-	console.log(formData)
-	console.log($("#file-0a")[0].files[0])
-	console.log($("#userName").attr('data-value'))
-	console.log($("#groupinfo").attr('data-value'))
-	console.log($("#showList").attr('data-gsno'))
+function ajaxAddAlbumPhoto() {
+	$('body').on('click','#scheduleList', function(e) {
+		var no = $(this).attr('data-gsno');
+		console.log(no)
 
-	$.ajax({
-		url: serverAddr + '/album/add.json',
-		processData: false,
-		contentType: false,
-		data: formData,
-		type: 'POST',
-		success: function(result){
-			console.log(result)
-		}
+		$(".al-btn").click(function(e) { 
+			/*ajaxAddGroup(makegroup)*/
+			console.log("111")
+
+			var formData = new FormData();
+			formData.append("memberNo", $("#userName").attr('data-value'));
+			formData.append("groupNo", $("#groupinfo").attr('data-value'));
+			formData.append("groupscNo", no);
+			formData.append("file1", $("#file-1")[0].files[0]);
+			console.log(formData)
+			console.log($("#file-1")[0].files[0])
+			console.log($("#userName").attr('data-value'))
+			console.log($("#groupinfo").attr('data-value'))
+			console.log($("#scheduleList").attr('data-gsno'))
+
+			$.ajax({
+				url: serverAddr + '/album/add.json',
+				processData: false,
+				contentType: false,
+				data: formData,
+				type: 'POST',
+				success: function(result){
+					console.log(result)
+				}
+			});
+		});
 	});
-
 };
 
 /*function ajaxImagesLoad() {
